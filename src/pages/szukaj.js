1a843164760cf44abc84b3a29d7a591ed5047933
Daniel Alberski <danielalberski@gmail.com>
2020-08-17 19:21:40 +0200

    google search engine

--- src/pages/szukaj.js
+++ src/pages/szukaj.js
@@ -1,11 +1,16 @@
 import React from "react"
 import Layout from "../layout/layout"
 import SEO from "../components/seo"
+import Helmet from 'react-helmet'
 
 const Page = () => (
   <Layout>
     <SEO title={"Szukaj"}/>
     <h1>Szukaj</h1>
+    <Helmet>
+      <script async src="https://cse.google.com/cse.js?cx=f819788bf799ec7f3" />
+    </Helmet>
+    <div className="gcse-search" />
   </Layout>
 )
 
