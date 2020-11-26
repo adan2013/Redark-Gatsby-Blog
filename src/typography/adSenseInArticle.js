b22cb3dfa890e608e0826f864c57e3d7225b3c5d
Daniel Alberski <danielalberski@gmail.com>
2020-11-26 14:49:35 +0100

    adsense cleanup

--- src/typography/adSenseInArticle.js
+++ src/typography/adSenseInArticle.js
@@ -3,33 +3,25 @@ import styled from 'styled-components'
 import siteConfig from '../site-config.json'
 
 const AdContainer = styled.div`
-
+  
 `
 
 const AdSenseInArticle = () => {
   React.useEffect(() => {
     if(typeof window !== "undefined") (window.adsbygoogle = window.adsbygoogle || []).push({})
   })
-  return(siteConfig.adSense && <AdContainer><GoogleTag/></AdContainer>)
+  return(
+    siteConfig.adSense
+    &&
+    <AdContainer>
+      <ins className="adsbygoogle"
+           style={{display: 'block', textAlign: 'center'}}
+           data-ad-layout="in-article"
+           data-ad-format="fluid"
+           data-ad-client="ca-pub-7672695014670144"
+           data-ad-slot="9778734203" />
+    </AdContainer>
+  )
 }
-
-// const AdBlockMessageContainer = styled.div`
-//
-// `
-
-// const adBlockMessage = () => (
-//   <AdBlockMessageContainer>
-//
-//   </AdBlockMessageContainer>
-// )
-
-const GoogleTag = () => (
-  <ins className="adsbygoogle"
-       style={{display: 'block', textAlign: 'center'}}
-       data-ad-layout="in-article"
-       data-ad-format="fluid"
-       data-ad-client="ca-pub-7672695014670144"
-       data-ad-slot="9778734203" />
-)
 
 export default AdSenseInArticle
