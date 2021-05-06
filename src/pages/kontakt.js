c9a96384f55d356213d137a148f2cbf049f8b850
Daniel Alberski <danielalberski@gmail.com>
2021-05-06 22:09:24 +0200

    contact page

--- src/pages/kontakt.js
+++ src/pages/kontakt.js
@@ -1,9 +1,30 @@
 import React, { useState } from "react"
 import styled from 'styled-components'
+import { graphql } from "gatsby"
+import Image from "gatsby-image"
 import Layout from "../layout/layout"
 import SEO from "../components/seo"
 import PageContent from "../typography"
 import siteConfig from "../site-config.json"
+
+const AboutMeContainer = styled.div`
+  display: flex;
+  align-items: center;
+  @media (max-width: ${props => props.theme.mobileBreakpoint}) {
+    align-items: stretch;
+    flex-direction: column;
+  }
+`
+
+const AboutMeImageColumn = styled.div`
+  flex: 2;
+  max-width: 380px;
+`
+
+const AboutMeTextColumn = styled.div`
+  flex: 3;
+  margin: 8px;
+`
 
 const List = styled.ul`
   list-style: none;
@@ -38,7 +59,7 @@ const getMailText = () => (
 )
 /* eslint-enable */
 
-const Page = () => {
+const Page = ({ data }) => {
   const [visible, setVisible] = useState(false)
 
   return(
@@ -46,11 +67,27 @@ const Page = () => {
       <SEO title={"Kontakt"}/>
       <h1>Kontakt</h1>
       <PageContent>
-        <h2>Dane kontaktowe</h2>
-        Kontakt z osobą odpowiedzialną za rozwój serwisu możliwy jest za pomocą poczty elektronicznej lub podanych poniżej mediów społecznościowych. Wszelkie potrzebne odnośniki znajdują się poniżej:
+        <h2>O mnie</h2>
+        <AboutMeContainer>
+          <AboutMeImageColumn>
+            <Image fluid={data.file.childImageSharp.fluid} alt={'autor redark.pl'} />
+          </AboutMeImageColumn>
+          <AboutMeTextColumn>
+            Nazywam się Daniel Alberski.
+            Interesuje się elektroniką, a w szczególności
+            programowaniem. Redark jest miejscem, gdzie
+            mogę rozwijać swoje pasje i dzielić się swoją wiedzą
+            z innymi. Obecnie skupiam się na zbudowaniu
+            kompletnej bazy informacji o składaniu i naprawianiu
+            komputerów. Zajmuję się również recenzowaniem sprzętu
+            elektronicznego oraz gier komputerowych.
+          </AboutMeTextColumn>
+        </AboutMeContainer>
+        <h2>Kontakt i współpraca</h2>
+        Kontakt możliwy jest za pomocą poczty elektronicznej lub portali społecznościowych podanych poniżej. Osoby zainteresowane współpracą zapraszam do kontaktu mailowego w celu otrzymania oferty oraz statystyk witryny.
         <List>
           {visible && <li>{getMailText()}</li>}
-          {!visible && <li><button onClick={() => setVisible(true)}>(kliknij tutaj, aby pokazać adres e-mail)</button></li>}
+          {!visible && <li><button onClick={() => setVisible(true)}>(kliknij tutaj, aby odsłonić adres e-mail)</button></li>}
           {
             siteConfig.social.map(item => (
               <li key={item.name}><a href={item.link} target={"_blank"} rel="noreferrer">{item.name}</a></li>
@@ -61,5 +98,17 @@ const Page = () => {
     </Layout>
   )
 }
+
+export const query = graphql`
+  query {
+    file(relativePath: { eq: "autor.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 380, quality: 80) {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+  }
+`
 
 export default Page
